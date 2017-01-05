a3a8b44fa69350a4d34a0b72ec0b3beff8e46031
Ysrael Rojas Rojas <yrra_rojas@hotmail.com>
2017-01-05 11:40:32 -0500

    platzigram - interfaces usuario con yo-yo agregado

--- src/index.js
+++ src/index.js
@@ -1,13 +1,54 @@
 var page = require('page');
+var yo = require('yo-yo');
+var empty = require('empty-element');
 
 var main = document.getElementById('main-container')
 
 page('/', function(ctx, next){
-	main.innerHTML = 'HOME <a href="/signup">SIGNUP</a>';
+	main.innerHTML = '<a href="/signup">SIGNUP</a>';
 })
 
 page('/signup', function(ctx, next){
-	main.innerHTML = 'SIGNUP <a href="/">HOME</a>';
+	var el = yo`<div class="container">
+			<div class="row">
+				<div class="col s10 push-s1">
+					<div class="row">
+						<div class="col m5 hide-on-small-only">
+							<img class="iphone" src="iphone.png">
+						</div>
+						<div class="col s12 m7">
+							<div class="row">
+								<div class="signup-box">
+									<h1 class="platzigram">Platzigram</h1>
+									<form class="signup-form">
+										<h2>Regístrate para ver fotos de tus amigos estudiando en Platzi</h2>
+										<div class="section">
+											<a class="btn btn-fb hide-on-small-only">Iniciar sesión con Facebook</a>
+											<a class="btn btn-fb hide-on-med-and-up">Iniciar sesión</a>
+										</div>
+										<div class="divider"></div>
+										<div class="section">
+											<input type="email" name="email" placeholder="Correo elecrónico">
+											<input type="text" name="name" placeholder="Nombre cmpleto">
+											<input type="text" name="username" placeholder="Nombre de usuario">
+											<input type="password" name="password" placeholder="Contraseña">
+											<button class="btn waves-effect waves-light btn-signup" type="submit">Regístrate</button>
+										</div>
+									</form>
+								</div>
+							</div>	
+							<div class="row">
+								<div class="login-box">
+									¿Tienes una cuenta? <a href="/signin">Entrar</a>
+								</div>
+							</div>												
+						</div>						
+					</div>	
+				</div>
+			</div>
+		</div>`
+
+		empty(main).appendChild(el);
 })
 
 page();
